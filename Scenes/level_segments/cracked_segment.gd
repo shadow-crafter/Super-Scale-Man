62236913ef70967bd871474c85842c15ef16e720
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2024-08-18 11:36:15 -0400

    Reduce cracked scale threshold

--- Scenes/level_segments/cracked_segment.gd
+++ Scenes/level_segments/cracked_segment.gd
@@ -4,6 +4,6 @@ extends LevelSegment
 @onready var hitbox: CollisionShape2D = $Area2D/CollisionShape2D
 
 func _on_hurtbox_area_entered(area: Area2D) -> void:
-	if area.get_parent().scale.x >= 6.0:
+	if area.get_parent().scale.x >= 5.0:
 		sprite.visible = false
 		hitbox.set_deferred("disabled", true)
